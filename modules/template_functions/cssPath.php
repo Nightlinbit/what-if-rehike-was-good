f43a249d6b94ed3436f513b44a340cd71365f694
Nightlinbit <dcoop2004@gmail.com>
2022-11-27 11:28:35 -0700

    fix css bug

--- modules/template_functions/cssPath.php
+++ modules/template_functions/cssPath.php
@@ -1,6 +1,7 @@
 <?php
 \Rehike\TemplateFunctions::register("cssPath", function($name, $pref, $constants) {
-    if (isset($pref -> f4) && substr($pref -> f4, 0, 1) == "4") 
+    if (isset($pref -> f4) && substr($pref -> f4, 0, 1) == "4" &&
+            isset($constants->{'css2x'}->{$name})) 
         $prefix = "css2x";
     else
         $prefix = "css";
